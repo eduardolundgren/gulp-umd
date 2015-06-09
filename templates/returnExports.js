6bb7cba96ddcc4a8c3c408441535b94081394f06
Sergey Kamardin <gobwas@gmail.com>
2015-06-09 13:06:18 +0300

    Update returnExports.js

--- templates/returnExports.js
+++ templates/returnExports.js
@@ -5,7 +5,7 @@
     module.exports = factory(<%= cjs %>);
   } else {
     var _root = root;
-    <% (Array.isArray(namespace) ? namespace : namespace.split(".")).forEach(function(name, index, list) { %><%if (index == (list.length - 1)) { %>_root["<%= name %>"] = factory(<%= global %>);<% } else { %>_root = _root["<%= name %>"] || {};<% } %><% }); %>
+    <% (Array.isArray(namespace) ? namespace : namespace.split(".")).forEach(function(name, index, list) { %><%if (index == (list.length - 1)) { %>_root["<%= name %>"] = factory(<%= global %>);<% } else { %>_root = (_root["<%= name %>"] = _root["<%= name %>"] || {});<% } %><% }); %>
   }
 }(this, function(<%= param %>) {
 <%= contents %>
